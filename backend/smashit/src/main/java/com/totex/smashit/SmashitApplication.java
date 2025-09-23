bb160c5078f7d24840beb0ab198e2dd73d328e48
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-23 09:44:16 -0300

    first commit

---
+++ backend/smashit/src/main/java/com/totex/smashit/SmashitApplication.java
@@ -0,0 +1,13 @@
+package com.totex.smashit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmashitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmashitApplication.class, args);
+	}
+
+}
---
+++ backend/smashit/src/test/java/com/totex/smashit/SmashitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totex.smashit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmashitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
