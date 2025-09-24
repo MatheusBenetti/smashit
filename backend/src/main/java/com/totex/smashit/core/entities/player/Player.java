2b34832308a891dd443a49444819d5099809c133
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-24 09:48:07 -0300

    created player model

---
+++ backend/src/main/java/com/totex/smashit/core/entities/player/Player.java
@@ -0,0 +1,20 @@
+package com.totex.smashit.core.entities.player;
+
+import com.totex.smashit.core.enums.PlayerLevel;
+
+import java.time.LocalDateTime;
+
+public record Player(
+        Long id,
+        String name,
+        String username,
+        String password,
+        String email,
+        String phone,
+        PlayerLevel playerLevel,
+        String city,
+        String state,
+        String country,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {}
