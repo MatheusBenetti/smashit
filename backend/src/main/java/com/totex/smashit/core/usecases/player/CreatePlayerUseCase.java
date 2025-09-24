87a73227780e9dba1ef2e0ab08b74ac1f10e09bf
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-24 09:47:28 -0300

    Add create player use case

---
+++ backend/src/main/java/com/totex/smashit/core/usecases/player/CreatePlayerUseCase.java
@@ -0,0 +1,8 @@
+package com.totex.smashit.core.usecases.player;
+
+import com.totex.smashit.core.entities.player.Player;
+
+public interface CreatePlayerUseCase {
+    public Player execute(Player player);
+
+}
