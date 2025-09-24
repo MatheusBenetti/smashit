27bebda9d749452c6c8908dc6cd47bde2c246e64
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-24 09:48:19 -0300

    created player entity

---
+++ backend/src/main/java/com/totex/smashit/infra/persistence/player/PlayerEntity.java
@@ -0,0 +1,53 @@
+package com.totex.smashit.infra.persistence.player;
+
+import com.totex.smashit.core.enums.PlayerLevel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "players")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlayerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    private PlayerLevel playerLevel;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String country;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
