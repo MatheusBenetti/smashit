058db24469bca939864270cd5f5be79acbc36c7c
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-24 09:48:37 -0300

    created player gateways

---
+++ backend/src/main/java/com/totex/smashit/core/gateway/player/PlayerGateway.java
@@ -0,0 +1,8 @@
+package com.totex.smashit.core.gateway.player;
+
+import com.totex.smashit.core.entities.player.Player;
+
+public interface PlayerGateway {
+
+    Player createPlayer(Player player);
+}
---
+++ backend/src/main/java/com/totex/smashit/infra/gateway/player/PlayerRepositoryGateway.java
@@ -0,0 +1,25 @@
+package com.totex.smashit.infra.gateway.player;
+
+import com.totex.smashit.core.entities.player.Player;
+import com.totex.smashit.core.gateway.player.PlayerGateway;
+import com.totex.smashit.infra.mapper.player.PlayerEntityMapper;
+import com.totex.smashit.infra.persistence.player.PlayerEntity;
+import com.totex.smashit.infra.persistence.player.PlayerRepository;
+
+public class PlayerRepositoryGateway implements PlayerGateway {
+
+    private final PlayerRepository playerRepository;
+    private final PlayerEntityMapper playerEntityMapper;
+
+    public PlayerRepositoryGateway(PlayerRepository playerRepository, PlayerEntityMapper playerEntityMapper) {
+        this.playerRepository = playerRepository;
+        this.playerEntityMapper = playerEntityMapper;
+    }
+
+    @Override
+    public Player createPlayer(Player player) {
+        PlayerEntity entity = playerEntityMapper.toEntity(player);
+        PlayerEntity newPlayer = playerRepository.save(entity);
+        return playerEntityMapper.toDomain(newPlayer);
+    }
+}
