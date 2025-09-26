9bc19d9cdc22c7c548f9b310c5141ec235e7283e
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-26 11:25:58 -0300

    create court entity mapper

---
+++ backend/src/main/java/com/totex/smashit/infra/mapper/court/CourtEntityMapper.java
@@ -0,0 +1,17 @@
+package com.totex.smashit.infra.mapper.court;
+
+import com.totex.smashit.core.entities.court.Court;
+import com.totex.smashit.infra.persistence.court.CourtEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourtEntityMapper {
+
+    public CourtEntity toEntity(Court court) {
+        return new CourtEntity(court.id(), court.name(), court.courtType());
+    }
+
+    public Court toDomain(CourtEntity courtEntity) {
+        return new Court(courtEntity.getId(), courtEntity.getName(), courtEntity.getCourtType());
+    }
+}
