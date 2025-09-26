9f362672032b8948752bd34cd76d5ea7172ec348
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-26 11:25:12 -0300

    adjust match model

--- backend/src/main/java/com/totex/smashit/core/entities/match/Match.java
+++ backend/src/main/java/com/totex/smashit/core/entities/match/Match.java
@@ -1,17 +1,20 @@
 package com.totex.smashit.core.entities.match;
 
 import com.totex.smashit.core.entities.court.Court;
+import com.totex.smashit.core.entities.player.Player;
 import com.totex.smashit.core.enums.MatchStatus;
 import com.totex.smashit.core.enums.MatchType;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 public record Match(
         Long id,
         LocalDate date,
         LocalTime hour,
         Court courtId,
+        List<Player> players,
         MatchStatus matchStatus,
         MatchType matchType
 ) {}
