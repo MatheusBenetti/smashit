cec689c88b55b672020632286a3bb9efc3add77e
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-26 11:26:20 -0300

    create match use case

---
+++ backend/src/main/java/com/totex/smashit/core/usecases/match/CreateMatchUseCase.java
@@ -0,0 +1,8 @@
+package com.totex.smashit.core.usecases.match;
+
+import com.totex.smashit.core.entities.match.Match;
+
+public interface CreateMatchUseCase {
+
+    public Match execute(Match match);
+}
