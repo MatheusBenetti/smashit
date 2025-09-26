4e652564f1ad0c2e5060b1206bce18c2e291d5b8
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-26 11:26:28 -0300

    create match use case implementation

---
+++ backend/src/main/java/com/totex/smashit/core/usecases/match/CreateMatchUseCaseImpl.java
@@ -0,0 +1,10 @@
+package com.totex.smashit.core.usecases.match;
+
+import com.totex.smashit.core.entities.match.Match;
+
+public class CreateMatchUseCaseImpl implements CreateMatchUseCase {
+    @Override
+    public Match execute(Match match) {
+        return null;
+    }
+}
