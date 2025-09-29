4a66576a4575dff2458cb80989cb8af83b791b82
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-29 10:45:27 -0300

    added repository annotation

--- backend/src/main/java/com/totex/smashit/infra/persistence/player/PlayerRepository.java
+++ backend/src/main/java/com/totex/smashit/infra/persistence/player/PlayerRepository.java
@@ -1,6 +1,8 @@
 package com.totex.smashit.infra.persistence.player;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
 }
