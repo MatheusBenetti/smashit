6f6e3d9a546ad2dbd2b2d8c847e53da8c2df4288
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-29 10:31:26 -0300

    add match queries to repository

--- backend/src/main/java/com/totex/smashit/infra/persistence/match/MatchRepository.java
+++ backend/src/main/java/com/totex/smashit/infra/persistence/match/MatchRepository.java
@@ -1,7 +1,21 @@
 package com.totex.smashit.infra.persistence.match;
 
 import com.totex.smashit.core.entities.match.Match;
+import com.totex.smashit.core.enums.CourtType;
+import com.totex.smashit.core.enums.MatchStatus;
+import com.totex.smashit.core.enums.PlayerLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
+    boolean existsByMatchStatus(MatchStatus matchStatus);
+
+    List<MatchEntity> findByMatchStatus(MatchStatus matchStatus);
+
+    List<MatchEntity> findByPlayerLevel(PlayerLevel playerLevel);
+
+    List<MatchEntity> findByCourtSurface(CourtType courtType);
 }
