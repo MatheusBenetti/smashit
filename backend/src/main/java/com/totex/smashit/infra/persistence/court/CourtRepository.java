b65b0c25f3da3b14657500d93f5c8faa0cdeddc6
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-29 10:47:41 -0300

    added repository annotation to court repository and extended jpa

--- backend/src/main/java/com/totex/smashit/infra/persistence/court/CourtRepository.java
+++ backend/src/main/java/com/totex/smashit/infra/persistence/court/CourtRepository.java
@@ -1,4 +1,8 @@
 package com.totex.smashit.infra.persistence.court;
 
-public class CourtRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourtRepository extends JpaRepository<CourtEntity, Long> {
 }
