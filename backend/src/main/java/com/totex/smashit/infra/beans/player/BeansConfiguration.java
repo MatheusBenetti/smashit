8c019466dcd0e044ab8fadab32d7cac545645a2f
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-29 10:47:54 -0300

    added beans configuration to player

---
+++ backend/src/main/java/com/totex/smashit/infra/beans/player/BeansConfiguration.java
@@ -0,0 +1,16 @@
+package com.totex.smashit.infra.beans.player;
+
+import com.totex.smashit.core.gateway.player.PlayerGateway;
+import com.totex.smashit.core.usecases.player.CreatePlayerUseCase;
+import com.totex.smashit.core.usecases.player.CreatePlayerUseCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeansConfiguration {
+
+    @Bean
+    public CreatePlayerUseCase createPlayerUseCase(PlayerGateway playerGateway) {
+        return new CreatePlayerUseCaseImpl(playerGateway);
+    }
+}
