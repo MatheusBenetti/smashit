d01ce9d24809c89658ddaf8055180e57d34a4ecd
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-29 10:46:30 -0300

    adjust match implementation

--- backend/src/main/java/com/totex/smashit/core/usecases/match/CreateMatchUseCaseImpl.java
+++ backend/src/main/java/com/totex/smashit/core/usecases/match/CreateMatchUseCaseImpl.java
@@ -1,10 +1,18 @@
 package com.totex.smashit.core.usecases.match;
 
 import com.totex.smashit.core.entities.match.Match;
+import com.totex.smashit.core.gateway.match.MatchGateway;
 
 public class CreateMatchUseCaseImpl implements CreateMatchUseCase {
+
+    private final MatchGateway matchGateway;
+
+    public CreateMatchUseCaseImpl(MatchGateway matchGateway) {
+        this.matchGateway = matchGateway;
+    }
+
     @Override
     public Match execute(Match match) {
-        return null;
+        return matchGateway.createMatch(match);
     }
 }
