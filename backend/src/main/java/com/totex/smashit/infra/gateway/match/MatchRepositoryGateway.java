73673342ece1ff8d6c5d486bba6f1ceca977da1d
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-29 10:45:43 -0300

    added match repository gateway

---
+++ backend/src/main/java/com/totex/smashit/infra/gateway/match/MatchRepositoryGateway.java
@@ -0,0 +1,25 @@
+package com.totex.smashit.infra.gateway.match;
+
+import com.totex.smashit.core.entities.match.Match;
+import com.totex.smashit.core.gateway.match.MatchGateway;
+import com.totex.smashit.infra.mapper.match.MatchEntityMapper;
+import com.totex.smashit.infra.persistence.match.MatchEntity;
+import com.totex.smashit.infra.persistence.match.MatchRepository;
+
+public class MatchRepositoryGateway implements MatchGateway {
+
+    private final MatchRepository matchRepository;
+    private final MatchEntityMapper matchEntityMapper;
+
+    public MatchRepositoryGateway(MatchRepository matchRepository, MatchEntityMapper matchEntityMapper) {
+        this.matchRepository = matchRepository;
+        this.matchEntityMapper = matchEntityMapper;
+    }
+
+    @Override
+    public Match createMatch(Match match) {
+        MatchEntity entity = matchEntityMapper.toEntity(match);
+        MatchEntity newMatch = matchRepository.save(entity);
+        return matchEntityMapper.toDomain(newMatch);
+    }
+}
