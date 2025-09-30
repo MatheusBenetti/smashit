e4d2eba1e4e14f9becb208f44f793b0756fce57a
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-30 09:35:22 -0300

    create court use case

---
+++ backend/src/main/java/com/totex/smashit/core/usecases/court/CreateCourtUseCase.java
@@ -0,0 +1,8 @@
+package com.totex.smashit.core.usecases.court;
+
+import com.totex.smashit.core.entities.court.Court;
+
+public interface CreateCourtUseCase {
+
+    public Court execute(Court court);
+}
