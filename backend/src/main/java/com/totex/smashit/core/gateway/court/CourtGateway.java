e6768f4da0026bcef844e0b4236d8fd050d775f9
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-30 09:35:34 -0300

    create court gateways

---
+++ backend/src/main/java/com/totex/smashit/core/gateway/court/CourtGateway.java
@@ -0,0 +1,8 @@
+package com.totex.smashit.core.gateway.court;
+
+import com.totex.smashit.core.entities.court.Court;
+
+public interface CourtGateway {
+
+    Court createCourt(Court court);
+}
---
+++ backend/src/main/java/com/totex/smashit/infra/gateway/court/CourtRepositoryGateway.java
@@ -0,0 +1,25 @@
+package com.totex.smashit.infra.gateway.court;
+
+import com.totex.smashit.core.entities.court.Court;
+import com.totex.smashit.core.gateway.court.CourtGateway;
+import com.totex.smashit.infra.mapper.court.CourtEntityMapper;
+import com.totex.smashit.infra.persistence.court.CourtEntity;
+import com.totex.smashit.infra.persistence.court.CourtRepository;
+
+public class CourtRepositoryGateway implements CourtGateway {
+
+    private final CourtRepository courtRepository;
+    private final CourtEntityMapper courtEntityMapper;
+
+    public CourtRepositoryGateway(CourtRepository courtRepository, CourtEntityMapper courtEntityMapper) {
+        this.courtRepository = courtRepository;
+        this.courtEntityMapper = courtEntityMapper;
+    }
+
+    @Override
+    public Court createCourt(Court court) {
+        CourtEntity entity = courtEntityMapper.toEntity(court);
+        CourtEntity newCourt = courtRepository.save(entity);
+        return courtEntityMapper.toDomain(newCourt);
+    }
+}
