4be35d63d99c6b0585a5f0fe10f75d343e5816cb
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-30 09:35:45 -0300

    create court dto

---
+++ backend/src/main/java/com/totex/smashit/infra/dto/CourtDto.java
@@ -0,0 +1,15 @@
+package com.totex.smashit.infra.dto;
+
+import com.totex.smashit.core.enums.CourtType;
+
+public record CourtDto(
+        Long id,
+        String name,
+        CourtType courtType,
+        String address,
+        String city,
+        String state,
+        String club
+        // double long
+        // double lat
+) {}
