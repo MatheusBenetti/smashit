aa0020b4ca8b12518e111a7d19802b1bff73ef13
MatheusBenetti <mbbenetti1@gmail.com>
2025-09-30 09:34:54 -0300

    adjust court entity mapper

--- backend/src/main/java/com/totex/smashit/infra/mapper/court/CourtEntityMapper.java
+++ backend/src/main/java/com/totex/smashit/infra/mapper/court/CourtEntityMapper.java
@@ -8,10 +8,28 @@ import org.springframework.stereotype.Component;
 public class CourtEntityMapper {
 
     public CourtEntity toEntity(Court court) {
-        return new CourtEntity(court.id(), court.name(), court.courtType());
+
+        return new CourtEntity(
+                court.id(),
+                court.name(),
+                court.courtType(),
+                court.address(),
+                court.city(),
+                court.state(),
+                court.club()
+        );
+
     }
 
     public Court toDomain(CourtEntity courtEntity) {
-        return new Court(courtEntity.getId(), courtEntity.getName(), courtEntity.getCourtType());
+        return new Court(
+                courtEntity.getId(),
+                courtEntity.getName(),
+                courtEntity.getCourtType(),
+                courtEntity.getAddress(),
+                courtEntity.getCity(),
+                courtEntity.getState(),
+                courtEntity.getClub()
+        );
     }
 }
