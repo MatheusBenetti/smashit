9ca5cdc9489ead99640e41a6016435df0391dde9
MatheusBenetti <mbbenetti1@gmail.com>
2025-10-02 09:50:56 -0300

    create match_player gateway

---
+++ backend/src/main/java/com/totex/smashit/core/gateway/match_player/MatchPlayerGateway.java
@@ -0,0 +1,8 @@
+package com.totex.smashit.core.gateway.match_player;
+
+import com.totex.smashit.core.entities.match_player.MatchPlayer;
+
+public interface MatchPlayerGateway {
+
+    MatchPlayer createMatchPlayer(MatchPlayer matchPlayer);
+}
