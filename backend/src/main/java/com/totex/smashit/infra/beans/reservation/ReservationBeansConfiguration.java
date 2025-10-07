f4bd5c8840d990f6bd431ac50a7b1607d5e1dbda
MatheusBenetti <mbbenetti1@gmail.com>
2025-10-07 09:43:44 -0300

    create reservation bean configuration

---
+++ backend/src/main/java/com/totex/smashit/infra/beans/reservation/ReservationBeansConfiguration.java
@@ -0,0 +1,16 @@
+package com.totex.smashit.infra.beans.reservation;
+
+import com.totex.smashit.core.gateway.reservation.ReservationGateway;
+import com.totex.smashit.core.usecases.reservation.CreateReservationUseCase;
+import com.totex.smashit.core.usecases.reservation.CreateReservationUseCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ReservationBeansConfiguration {
+
+    @Bean
+    public CreateReservationUseCase createReservationUseCase(ReservationGateway reservationGateway) {
+        return new CreateReservationUseCaseImpl(reservationGateway);
+    }
+}
