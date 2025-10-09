3c25992014080b517e279ec5d103000cf0ceb8fa
MatheusBenetti <mbbenetti1@gmail.com>
2025-10-09 10:43:29 -0300

    created createReservation endpoint

--- backend/src/main/java/com/totex/smashit/infra/presentation/reservation/ReservationController.java
+++ backend/src/main/java/com/totex/smashit/infra/presentation/reservation/ReservationController.java
@@ -1,9 +1,35 @@
 package com.totex.smashit.infra.presentation.reservation;
 
+import com.totex.smashit.core.entities.reservation.Reservation;
+import com.totex.smashit.core.usecases.reservation.CreateReservationUseCase;
+import com.totex.smashit.infra.dto.ReservationDto;
+import com.totex.smashit.infra.mapper.reservation.ReservationMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/")
 public class ReservationController {
+
+    private final CreateReservationUseCase createReservationUseCase;
+    private final ReservationMapper reservationMapper;
+
+    public ReservationController(CreateReservationUseCase createReservationUseCase, ReservationMapper reservationMapper) {
+        this.createReservationUseCase = createReservationUseCase;
+        this.reservationMapper = reservationMapper;
+    }
+
+    public ResponseEntity<Map<String, Object>> createReservation(@RequestBody ReservationDto reservationDto) {
+        Reservation newReservation = createReservationUseCase.execute(reservationMapper.toDomain(reservationDto));
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("Message: ", "Reservation created successfully!");
+        response.put("Reservatio  data: ", reservationMapper.toDto(newReservation));
+        return ResponseEntity.ok(response);
+    }
 }
