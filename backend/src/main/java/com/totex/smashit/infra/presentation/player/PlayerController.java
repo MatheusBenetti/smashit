9d33e044d41d072d6c2a6922b5dd9eb429d54897
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-15 09:24:18 -0300

    added find player endpoint to controller

--- backend/src/main/java/com/totex/smashit/infra/presentation/player/PlayerController.java
+++ backend/src/main/java/com/totex/smashit/infra/presentation/player/PlayerController.java
@@ -2,26 +2,30 @@ package com.totex.smashit.infra.presentation.player;
 
 import com.totex.smashit.core.entities.player.Player;
 import com.totex.smashit.core.usecases.player.CreatePlayerUseCase;
+import com.totex.smashit.core.usecases.player.FindPlayerUseCase;
 import com.totex.smashit.infra.dto.PlayerDto;
 import com.totex.smashit.infra.mapper.player.PlayerMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/")
 public class PlayerController {
 
     private final CreatePlayerUseCase createPlayerUseCase;
+    private final FindPlayerUseCase findPlayerUseCase;
     private final PlayerMapper playerMapper;
 
-    public PlayerController(CreatePlayerUseCase createPlayerUseCase, PlayerMapper playerMapper) {
+    public PlayerController(CreatePlayerUseCase createPlayerUseCase,
+                            FindPlayerUseCase findPlayerUseCase,
+                            PlayerMapper playerMapper) {
         this.createPlayerUseCase = createPlayerUseCase;
+        this.findPlayerUseCase = findPlayerUseCase;
         this.playerMapper = playerMapper;
     }
 
@@ -33,5 +37,10 @@ public class PlayerController {
         response.put("Message: ", "Player added successfully!");
         response.put("Player data: ", playerMapper.toDto(newPlayer));
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("find-players")
+    public List<PlayerDto> findPlayers() {
+        return findPlayerUseCase.execute().stream().map(playerMapper::toDto).collect(Collectors.toList());
     }
 }
