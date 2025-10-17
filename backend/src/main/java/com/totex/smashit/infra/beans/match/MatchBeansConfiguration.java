18fa82955e32fca1fe32419d04c9543eb18d52d6
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-17 09:32:12 -0300

    create find matches service

--- backend/src/main/java/com/totex/smashit/infra/beans/match/MatchBeansConfiguration.java
+++ backend/src/main/java/com/totex/smashit/infra/beans/match/MatchBeansConfiguration.java
@@ -3,6 +3,8 @@ package com.totex.smashit.infra.beans.match;
 import com.totex.smashit.core.gateway.match.MatchGateway;
 import com.totex.smashit.core.usecases.match.CreateMatchUseCase;
 import com.totex.smashit.core.usecases.match.CreateMatchUseCaseImpl;
+import com.totex.smashit.core.usecases.match.FindMatchUseCase;
+import com.totex.smashit.core.usecases.match.FindMatchUseCaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +14,10 @@ public class MatchBeansConfiguration {
     @Bean
     public CreateMatchUseCase createMatchUseCase(MatchGateway matchGateway) {
         return new CreateMatchUseCaseImpl(matchGateway);
+    }
+
+    @Bean
+    public FindMatchUseCase findMatchUseCase(MatchGateway matchGateway) {
+        return new FindMatchUseCaseImpl(matchGateway);
     }
 }
