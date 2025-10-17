a0888188058087a9173487e81b28202133cbe913
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-17 09:32:32 -0300

    added find matches endpoint

--- backend/src/main/java/com/totex/smashit/infra/presentation/match/MatchController.java
+++ backend/src/main/java/com/totex/smashit/infra/presentation/match/MatchController.java
@@ -2,26 +2,30 @@ package com.totex.smashit.infra.presentation.match;
 
 import com.totex.smashit.core.entities.match.Match;
 import com.totex.smashit.core.usecases.match.CreateMatchUseCase;
+import com.totex.smashit.core.usecases.match.FindMatchUseCase;
 import com.totex.smashit.infra.dto.MatchDto;
 import com.totex.smashit.infra.mapper.match.MatchMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/")
 public class MatchController {
 
     private final CreateMatchUseCase createMatchUseCase;
+    private final FindMatchUseCase findMatchUseCase;
     private final MatchMapper matchMapper;
 
-    public MatchController(CreateMatchUseCase createMatchUseCase, MatchMapper matchMapper) {
+    public MatchController(CreateMatchUseCase createMatchUseCase,
+                           FindMatchUseCase findMatchUseCase,
+                           MatchMapper matchMapper) {
         this.createMatchUseCase = createMatchUseCase;
+        this.findMatchUseCase = findMatchUseCase;
         this.matchMapper = matchMapper;
     }
 
@@ -33,5 +37,10 @@ public class MatchController {
         response.put("Message: ", "Match created successfully!");
         response.put("Match data: ", matchMapper.toDto(newMatch));
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("find-matches")
+    public List<MatchDto> findGames() {
+        return findMatchUseCase.execute().stream().map(matchMapper::toDto).collect(Collectors.toList());
     }
 }
