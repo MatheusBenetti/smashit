433dccd4403718ec422034e2e0ed78bc2ccffbf5
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-17 09:32:41 -0300

    added find matches to gateway

--- backend/src/main/java/com/totex/smashit/core/gateway/match/MatchGateway.java
+++ backend/src/main/java/com/totex/smashit/core/gateway/match/MatchGateway.java
@@ -2,7 +2,10 @@ package com.totex.smashit.core.gateway.match;
 
 import com.totex.smashit.core.entities.match.Match;
 
+import java.util.List;
+
 public interface MatchGateway {
 
     Match createMatch(Match match);
+    List<Match> findMatch();
 }
