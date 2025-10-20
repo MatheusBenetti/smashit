374c4b84144bc07449f1a29c0455a27637cadf09
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-20 09:42:41 -0300

    add find courts to court gateway

--- backend/src/main/java/com/totex/smashit/core/gateway/court/CourtGateway.java
+++ backend/src/main/java/com/totex/smashit/core/gateway/court/CourtGateway.java
@@ -2,7 +2,10 @@ package com.totex.smashit.core.gateway.court;
 
 import com.totex.smashit.core.entities.court.Court;
 
+import java.util.List;
+
 public interface CourtGateway {
 
     Court createCourt(Court court);
+    List<Court> findCourts();
 }
