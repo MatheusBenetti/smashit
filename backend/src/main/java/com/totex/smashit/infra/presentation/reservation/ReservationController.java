38f31a0d43f6c86674b0fb3c6899c1f05eaf17a9
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-20 09:47:29 -0300

    add post mapping annotation to create reservation

--- backend/src/main/java/com/totex/smashit/infra/presentation/reservation/ReservationController.java
+++ backend/src/main/java/com/totex/smashit/infra/presentation/reservation/ReservationController.java
@@ -5,6 +5,7 @@ import com.totex.smashit.core.usecases.reservation.CreateReservationUseCase;
 import com.totex.smashit.infra.dto.ReservationDto;
 import com.totex.smashit.infra.mapper.reservation.ReservationMapper;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ public class ReservationController {
         this.reservationMapper = reservationMapper;
     }
 
+    @PostMapping("create-reservation")
     public ResponseEntity<Map<String, Object>> createReservation(@RequestBody ReservationDto reservationDto) {
         Reservation newReservation = createReservationUseCase.execute(reservationMapper.toDomain(reservationDto));
         Map<String, Object> response = new HashMap<>();
