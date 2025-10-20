0c3b52752681e45d1117f56ad1c5880ac155b572
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-20 09:44:05 -0300

    add find courts to service

--- backend/src/main/java/com/totex/smashit/infra/beans/court/CourtBeansConfiguration.java
+++ backend/src/main/java/com/totex/smashit/infra/beans/court/CourtBeansConfiguration.java
@@ -3,6 +3,8 @@ package com.totex.smashit.infra.beans.court;
 import com.totex.smashit.core.gateway.court.CourtGateway;
 import com.totex.smashit.core.usecases.court.CreateCourtUseCase;
 import com.totex.smashit.core.usecases.court.CreateCourtUseCaseImpl;
+import com.totex.smashit.core.usecases.court.FindCourtUseCase;
+import com.totex.smashit.core.usecases.court.FindCourtUseCaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +14,10 @@ public class CourtBeansConfiguration {
     @Bean
     public CreateCourtUseCase createCourtUseCase(CourtGateway courtGateway) {
         return new CreateCourtUseCaseImpl(courtGateway);
+    }
+
+    @Bean
+    public FindCourtUseCase findCourtUseCase(CourtGateway courtGateway) {
+        return new FindCourtUseCaseImpl(courtGateway);
     }
 }
