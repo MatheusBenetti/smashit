bbb62f5f38479e8733310ae87c5306596aeff6f8
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-20 09:50:24 -0300

    create find courts endpoint

--- backend/src/main/java/com/totex/smashit/infra/presentation/court/CourtController.java
+++ backend/src/main/java/com/totex/smashit/infra/presentation/court/CourtController.java
@@ -2,25 +2,33 @@ package com.totex.smashit.infra.presentation.court;
 
 import com.totex.smashit.core.entities.court.Court;
 import com.totex.smashit.core.usecases.court.CreateCourtUseCase;
+import com.totex.smashit.core.usecases.court.FindCourtUseCase;
 import com.totex.smashit.infra.dto.CourtDto;
 import com.totex.smashit.infra.mapper.court.CourtMapper;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/")
 public class CourtController {
 
     private final CreateCourtUseCase createCourtUseCase;
+    private final FindCourtUseCase findCourtUseCase;
     private final CourtMapper courtMapper;
 
-    public CourtController(CreateCourtUseCase createCourtUseCase, CourtMapper courtMapper) {
+    public CourtController(CreateCourtUseCase createCourtUseCase,
+                           FindCourtUseCase findCourtUseCase,
+                           CourtMapper courtMapper) {
         this.createCourtUseCase = createCourtUseCase;
+        this.findCourtUseCase = findCourtUseCase;
         this.courtMapper = courtMapper;
     }
 
@@ -34,5 +42,8 @@ public class CourtController {
         return ResponseEntity.ok(response);
     }
 
-
+    @GetMapping("find-courts")
+    public List<CourtDto> findCourts() {
+        return findCourtUseCase.execute().stream().map(courtMapper::toDto).collect(Collectors.toList());
+    }
 }
