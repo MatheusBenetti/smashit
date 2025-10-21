74a9267dd242954fd8e30d64d3d9f5866dd26488
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-21 09:20:02 -0300

    create find reservation endpoint

--- backend/src/main/java/com/totex/smashit/infra/presentation/reservation/ReservationController.java
+++ backend/src/main/java/com/totex/smashit/infra/presentation/reservation/ReservationController.java
@@ -2,26 +2,30 @@ package com.totex.smashit.infra.presentation.reservation;
 
 import com.totex.smashit.core.entities.reservation.Reservation;
 import com.totex.smashit.core.usecases.reservation.CreateReservationUseCase;
+import com.totex.smashit.core.usecases.reservation.FindReservationsUseCase;
 import com.totex.smashit.infra.dto.ReservationDto;
 import com.totex.smashit.infra.mapper.reservation.ReservationMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/")
 public class ReservationController {
 
     private final CreateReservationUseCase createReservationUseCase;
+    private final FindReservationsUseCase findReservationsUseCase;
     private final ReservationMapper reservationMapper;
 
-    public ReservationController(CreateReservationUseCase createReservationUseCase, ReservationMapper reservationMapper) {
+    public ReservationController(CreateReservationUseCase createReservationUseCase,
+                                 FindReservationsUseCase findReservationsUseCase,
+                                 ReservationMapper reservationMapper) {
         this.createReservationUseCase = createReservationUseCase;
+        this.findReservationsUseCase = findReservationsUseCase;
         this.reservationMapper = reservationMapper;
     }
 
@@ -33,5 +37,10 @@ public class ReservationController {
         response.put("Message: ", "Reservation created successfully!");
         response.put("Reservation data: ", reservationMapper.toDto(newReservation));
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("find-reservations")
+    public List<ReservationDto> findReservations() {
+        return findReservationsUseCase.execute().stream().map(reservationMapper::toDto).collect(Collectors.toList());
     }
 }
