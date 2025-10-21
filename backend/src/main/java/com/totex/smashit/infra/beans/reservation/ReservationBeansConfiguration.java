205a5b3e8bc05fe23a27d2bc562091d254ec3a4e
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-21 09:21:01 -0300

    add find reservations to service

--- backend/src/main/java/com/totex/smashit/infra/beans/reservation/ReservationBeansConfiguration.java
+++ backend/src/main/java/com/totex/smashit/infra/beans/reservation/ReservationBeansConfiguration.java
@@ -3,6 +3,8 @@ package com.totex.smashit.infra.beans.reservation;
 import com.totex.smashit.core.gateway.reservation.ReservationGateway;
 import com.totex.smashit.core.usecases.reservation.CreateReservationUseCase;
 import com.totex.smashit.core.usecases.reservation.CreateReservationUseCaseImpl;
+import com.totex.smashit.core.usecases.reservation.FindReservationsUseCase;
+import com.totex.smashit.core.usecases.reservation.FindReservationsUseCaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +14,10 @@ public class ReservationBeansConfiguration {
     @Bean
     public CreateReservationUseCase createReservationUseCase(ReservationGateway reservationGateway) {
         return new CreateReservationUseCaseImpl(reservationGateway);
+    }
+
+    @Bean
+    public FindReservationsUseCase findReservationsUseCase(ReservationGateway reservationGateway) {
+        return new FindReservationsUseCaseImpl(reservationGateway);
     }
 }
