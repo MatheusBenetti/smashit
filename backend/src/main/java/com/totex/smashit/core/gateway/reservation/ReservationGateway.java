58fff7153dca6ae2b9f5697086b90422a02287d1
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-21 09:20:15 -0300

    create find reservation gateway

--- backend/src/main/java/com/totex/smashit/core/gateway/reservation/ReservationGateway.java
+++ backend/src/main/java/com/totex/smashit/core/gateway/reservation/ReservationGateway.java
@@ -2,7 +2,10 @@ package com.totex.smashit.core.gateway.reservation;
 
 import com.totex.smashit.core.entities.reservation.Reservation;
 
+import java.util.List;
+
 public interface ReservationGateway {
 
     public Reservation createReservation(Reservation reservation);
+    public List<Reservation> findReservations();
 }
