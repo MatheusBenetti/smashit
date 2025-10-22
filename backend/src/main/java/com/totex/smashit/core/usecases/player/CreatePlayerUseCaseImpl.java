81b29029c9519cfc756355103a4dfce9c0e00afd
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-22 09:27:39 -0300

    adding loggers to player use case

--- backend/src/main/java/com/totex/smashit/core/usecases/player/CreatePlayerUseCaseImpl.java
+++ backend/src/main/java/com/totex/smashit/core/usecases/player/CreatePlayerUseCaseImpl.java
@@ -3,7 +3,12 @@ package com.totex.smashit.core.usecases.player;
 import com.totex.smashit.core.entities.player.Player;
 import com.totex.smashit.core.gateway.player.PlayerGateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CreatePlayerUseCaseImpl implements CreatePlayerUseCase {
+
+    private static final Logger log = LoggerFactory.getLogger(CreatePlayerUseCaseImpl.class);
 
     private final PlayerGateway playerGateway;
 
@@ -13,6 +18,14 @@ public class CreatePlayerUseCaseImpl implements CreatePlayerUseCase {
 
     @Override
     public Player execute(Player player) {
-        return playerGateway.createPlayer(player);
+        log.info("Creating player: {}", player);
+        try {
+            Player created = playerGateway.createPlayer(player);
+            log.info("Player {} created successfully!", player.name());
+            return created;
+        } catch (Exception e) {
+            log.error("Error while creating player: {}", e.getMessage());
+            throw e;
+        }
     }
 }
