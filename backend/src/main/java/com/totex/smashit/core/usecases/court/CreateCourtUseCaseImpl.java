67efacb388b13ca29c3b653057d2b7679d7962c0
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-23 10:22:37 -0300

    adding loggers to court use case

--- backend/src/main/java/com/totex/smashit/core/usecases/court/CreateCourtUseCaseImpl.java
+++ backend/src/main/java/com/totex/smashit/core/usecases/court/CreateCourtUseCaseImpl.java
@@ -3,7 +3,12 @@ package com.totex.smashit.core.usecases.court;
 import com.totex.smashit.core.entities.court.Court;
 import com.totex.smashit.core.gateway.court.CourtGateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CreateCourtUseCaseImpl implements CreateCourtUseCase {
+
+    public static final Logger log = LoggerFactory.getLogger(CreateCourtUseCaseImpl.class);
 
     private final CourtGateway courtGateway;
 
@@ -13,6 +18,14 @@ public class CreateCourtUseCaseImpl implements CreateCourtUseCase {
 
     @Override
     public Court execute(Court court) {
-        return courtGateway.createCourt(court);
+        log.info("Creating court: {}", court);
+        try {
+            Court created = courtGateway.createCourt(court);
+            log.info("Court: {}, Address: {} created successfully!", court.name(), court.address());
+            return created;
+        } catch (Exception e) {
+            log.error("Error while creating court: {}", e.getMessage());
+            throw e;
+        }
     }
 }
