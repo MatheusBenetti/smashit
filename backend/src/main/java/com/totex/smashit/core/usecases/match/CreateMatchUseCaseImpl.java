962af86e35315d1b02330158811f0d51f3ca274e
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-10-27 11:23:14 -0300

    adding loggers to match use case

--- backend/src/main/java/com/totex/smashit/core/usecases/match/CreateMatchUseCaseImpl.java
+++ backend/src/main/java/com/totex/smashit/core/usecases/match/CreateMatchUseCaseImpl.java
@@ -3,7 +3,12 @@ package com.totex.smashit.core.usecases.match;
 import com.totex.smashit.core.entities.match.Match;
 import com.totex.smashit.core.gateway.match.MatchGateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CreateMatchUseCaseImpl implements CreateMatchUseCase {
+
+    private static final Logger log = LoggerFactory.getLogger(CreateMatchUseCaseImpl.class);
 
     private final MatchGateway matchGateway;
 
@@ -13,6 +18,14 @@ public class CreateMatchUseCaseImpl implements CreateMatchUseCase {
 
     @Override
     public Match execute(Match match) {
-        return matchGateway.createMatch(match);
+        log.info("Creating a tennis match: {}", match);
+        try {
+            Match created = matchGateway.createMatch(match);
+            log.info("Match {} created. Date: {}, Hour: {}", match.id(), match.date(), match.hour());
+            return created;
+        } catch (Exception e) {
+            log.error("Error occurred while creating a match: {}", e.getMessage());
+            throw e;
+        }
     }
 }
