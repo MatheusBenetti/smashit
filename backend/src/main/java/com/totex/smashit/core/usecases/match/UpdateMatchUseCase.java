d367a50d4d8f4f0694c4c0cd7b90252a99467770
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-11-04 11:31:17 -0300

    create update match use case

---
+++ backend/src/main/java/com/totex/smashit/core/usecases/match/UpdateMatchUseCase.java
@@ -0,0 +1,8 @@
+package com.totex.smashit.core.usecases.match;
+
+import com.totex.smashit.core.entities.match.Match;
+
+public interface UpdateMatchUseCase {
+
+    public Match execute(Match match);
+}
