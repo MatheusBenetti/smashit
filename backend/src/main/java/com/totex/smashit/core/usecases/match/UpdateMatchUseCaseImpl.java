541964924762b8eb002d369579a13b28ae6ca50a
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-11-04 11:31:09 -0300

    create update match use case implementation

---
+++ backend/src/main/java/com/totex/smashit/core/usecases/match/UpdateMatchUseCaseImpl.java
@@ -0,0 +1,11 @@
+package com.totex.smashit.core.usecases.match;
+
+import com.totex.smashit.core.entities.player.Player;
+import com.totex.smashit.core.usecases.player.UpdatePlayerUseCase;
+
+public class UpdateMatchUseCaseImpl implements UpdatePlayerUseCase {
+    @Override
+    public Player execute(Player player) {
+        return null;
+    }
+}
