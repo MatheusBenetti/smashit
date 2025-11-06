f9f0e8162257e26ae000da390a0e5f39677ee6d1
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-11-06 09:56:23 -0300

    add loggers to update court impl

--- backend/src/main/java/com/totex/smashit/core/usecases/court/UpdateCourtUseCaseImpl.java
+++ backend/src/main/java/com/totex/smashit/core/usecases/court/UpdateCourtUseCaseImpl.java
@@ -1,5 +1,6 @@
 package com.totex.smashit.core.usecases.court;
 
+import com.totex.smashit.core.entities.court.Court;
 import com.totex.smashit.core.gateway.court.CourtGateway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,5 +13,18 @@ public class UpdateCourtUseCaseImpl implements UpdateCourtUseCase {
 
     public UpdateCourtUseCaseImpl(CourtGateway courtGateway) {
         this.courtGateway = courtGateway;
+    }
+
+    @Override
+    public Court execute(Court court) {
+        log.info("Updating court: {}", court.id());
+        try {
+            Court updatedCourt = courtGateway.updateCourt(court);
+            log.info("Court updated successfully: {}", updatedCourt);
+            return updatedCourt;
+        } catch (Exception e) {
+            log.error("Error while updating court ID: {}, error: {}", court.id(), e.getMessage());
+            throw e;
+        }
     }
 }
