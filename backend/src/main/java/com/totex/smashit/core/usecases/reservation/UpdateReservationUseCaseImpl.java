8a14face15d638db279c8bf42223a1dcfd475af1
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-11-07 09:31:08 -0300

    add update reservation use case implementation

---
+++ backend/src/main/java/com/totex/smashit/core/usecases/reservation/UpdateReservationUseCaseImpl.java
@@ -0,0 +1,10 @@
+package com.totex.smashit.core.usecases.reservation;
+
+import com.totex.smashit.core.entities.reservation.Reservation;
+
+public class UpdateReservationUseCaseImpl implements UpdateReservationUseCase {
+    @Override
+    public Reservation execute(Reservation reservation) {
+        return null;
+    }
+}
