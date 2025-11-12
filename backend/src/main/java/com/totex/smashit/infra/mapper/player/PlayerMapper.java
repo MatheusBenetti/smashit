5ba8d4c731b0f4bff12e63ca4c58116325265aff
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-11-12 09:33:01 -0300

    add new mapper to update player

--- backend/src/main/java/com/totex/smashit/infra/mapper/player/PlayerMapper.java
+++ backend/src/main/java/com/totex/smashit/infra/mapper/player/PlayerMapper.java
@@ -2,7 +2,10 @@ package com.totex.smashit.infra.mapper.player;
 
 import com.totex.smashit.core.entities.player.Player;
 import com.totex.smashit.infra.dto.player.PlayerDto;
+import com.totex.smashit.infra.dto.player.UpdatePlayerRequest;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class PlayerMapper {
@@ -42,6 +45,26 @@ public class PlayerMapper {
                 playerDto.wins(),
                 playerDto.losses(),
                 playerDto.winRate()
+        );
+    }
+
+    public Player updateToDomain(UpdatePlayerRequest updatePlayerRequest) {
+        return new Player(
+                null,
+                updatePlayerRequest.name(),
+                updatePlayerRequest.username(),
+                null,
+                updatePlayerRequest.email(),
+                updatePlayerRequest.phone(),
+                updatePlayerRequest.playerLevel(),
+                updatePlayerRequest.city(),
+                updatePlayerRequest.state(),
+                updatePlayerRequest.country(),
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                updatePlayerRequest.wins(),
+                updatePlayerRequest.losses(),
+                updatePlayerRequest.winRate()
         );
     }
 }
