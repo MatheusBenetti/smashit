1edebd1922f7aace6cd81e74c2c3a89a73e011a2
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-11-12 09:32:48 -0300

    add update player to controller

--- backend/src/main/java/com/totex/smashit/infra/presentation/player/PlayerController.java
+++ backend/src/main/java/com/totex/smashit/infra/presentation/player/PlayerController.java
@@ -3,7 +3,9 @@ package com.totex.smashit.infra.presentation.player;
 import com.totex.smashit.core.entities.player.Player;
 import com.totex.smashit.core.usecases.player.CreatePlayerUseCase;
 import com.totex.smashit.core.usecases.player.FindPlayerUseCase;
+import com.totex.smashit.core.usecases.player.UpdatePlayerUseCase;
 import com.totex.smashit.infra.dto.player.PlayerDto;
+import com.totex.smashit.infra.dto.player.UpdatePlayerRequest;
 import com.totex.smashit.infra.mapper.player.PlayerMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,13 +25,16 @@ public class PlayerController {
 
     private final CreatePlayerUseCase createPlayerUseCase;
     private final FindPlayerUseCase findPlayerUseCase;
+    private final UpdatePlayerUseCase updatePlayerUseCase;
     private final PlayerMapper playerMapper;
 
     public PlayerController(CreatePlayerUseCase createPlayerUseCase,
                             FindPlayerUseCase findPlayerUseCase,
+                            UpdatePlayerUseCase updatePlayerUseCase,
                             PlayerMapper playerMapper) {
         this.createPlayerUseCase = createPlayerUseCase;
         this.findPlayerUseCase = findPlayerUseCase;
+        this.updatePlayerUseCase = updatePlayerUseCase;
         this.playerMapper = playerMapper;
     }
 
@@ -53,5 +58,24 @@ public class PlayerController {
     @GetMapping("find-players")
     public List<PlayerDto> findPlayers() {
         return findPlayerUseCase.execute().stream().map(playerMapper::toDto).collect(Collectors.toList());
+    }
+
+    @PutMapping("update-player/{id}")
+    public ResponseEntity<Map<String, Object>> updatePlayer(@PathVariable Long id, @RequestBody UpdatePlayerRequest updatePlayerRequest) {
+        log.info("Received request to update player with ID: {}", id);
+        try {
+            Player updatedPlayer = updatePlayerUseCase.execute(id, playerMapper.updateToDomain(updatePlayerRequest));
+
+            Map<String, Object> response = new HashMap<>();
+
+            response.put("message", "Player updated successfully!");
+            response.put("player", playerMapper.toDto(updatedPlayer));
+
+            log.info("Player with ID {} updated successfully", id);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            log.error("Error updating player with ID: {}. Error: {}", id, e.getMessage());
+            throw e;
+        }
     }
 }
