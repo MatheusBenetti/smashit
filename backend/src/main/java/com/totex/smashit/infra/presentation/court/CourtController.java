dd81c2e4e39747fb1d9fece7b95729e0f68e4948
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-11-13 10:10:52 -0300

    implement update court on controller

--- backend/src/main/java/com/totex/smashit/infra/presentation/court/CourtController.java
+++ backend/src/main/java/com/totex/smashit/infra/presentation/court/CourtController.java
@@ -3,13 +3,12 @@ package com.totex.smashit.infra.presentation.court;
 import com.totex.smashit.core.entities.court.Court;
 import com.totex.smashit.core.usecases.court.CreateCourtUseCase;
 import com.totex.smashit.core.usecases.court.FindCourtUseCase;
-import com.totex.smashit.infra.dto.CourtDto;
+import com.totex.smashit.core.usecases.court.UpdateCourtUseCase;
+import com.totex.smashit.infra.dto.court.CourtDto;
+import com.totex.smashit.infra.dto.court.UpdateCourtRequest;
 import com.totex.smashit.infra.mapper.court.CourtMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -26,18 +25,21 @@ public class CourtController {
 
     private final CreateCourtUseCase createCourtUseCase;
     private final FindCourtUseCase findCourtUseCase;
+    private final UpdateCourtUseCase updateCourtUseCase;
     private final CourtMapper courtMapper;
 
     public CourtController(CreateCourtUseCase createCourtUseCase,
                            FindCourtUseCase findCourtUseCase,
+                           UpdateCourtUseCase updateCourtUseCase,
                            CourtMapper courtMapper) {
         this.createCourtUseCase = createCourtUseCase;
         this.findCourtUseCase = findCourtUseCase;
+        this.updateCourtUseCase = updateCourtUseCase;
         this.courtMapper = courtMapper;
     }
 
     @PostMapping("create-court")
-    public ResponseEntity<Map<String, Object>> createCourt(CourtDto courtDto) {
+    public ResponseEntity<Map<String, Object>> createCourt(@RequestBody CourtDto courtDto) {
         log.info("Creating court: {}", courtDto);
         try {
             Court newCourt = createCourtUseCase.execute(courtMapper.toDomain(courtDto));
@@ -56,5 +58,23 @@ public class CourtController {
     @GetMapping("find-courts")
     public List<CourtDto> findCourts() {
         return findCourtUseCase.execute().stream().map(courtMapper::toDto).collect(Collectors.toList());
+    }
+
+    @PutMapping("update-court/{id}")
+    public ResponseEntity<Map<String, Object>> updateCourt(@PathVariable Long id, @RequestBody UpdateCourtRequest updateCourtRequest) {
+        log.info("Received request to update player with ID: {}", id);
+        try {
+            Court updatedCourt = updateCourtUseCase.execute(id, courtMapper.entityToDomain(updateCourtRequest));
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("Message: ", "Court updated successfully!");
+            response.put("Court: ", courtMapper.toDto(updatedCourt));
+
+            log.info("Court with ID {} updated successfully", id);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            log.error("Error updating player with ID: {}. Error: {}", id, e.getMessage());
+            throw e;
+        }
     }
 }
