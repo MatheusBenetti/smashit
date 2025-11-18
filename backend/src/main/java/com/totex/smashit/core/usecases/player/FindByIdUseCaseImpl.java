c9fbd658a492611a64addcb6d0e1e695cf865489
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-11-18 09:44:00 -0300

    add find by id use case implementation

---
+++ backend/src/main/java/com/totex/smashit/core/usecases/player/FindByIdUseCaseImpl.java
@@ -0,0 +1,10 @@
+package com.totex.smashit.core.usecases.player;
+
+import com.totex.smashit.core.entities.player.Player;
+
+public class FindByIdUseCaseImpl implements FindByIdUseCase {
+    @Override
+    public Player findById(Long id) {
+        return null;
+    }
+}
