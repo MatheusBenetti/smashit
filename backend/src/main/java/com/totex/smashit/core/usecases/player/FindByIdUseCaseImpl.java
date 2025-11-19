adaf95f2c1b1396e1e6c5bd08055e32b854218bc
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-11-19 09:14:05 -0300

    add dependency injection

--- backend/src/main/java/com/totex/smashit/core/usecases/player/FindByIdUseCaseImpl.java
+++ backend/src/main/java/com/totex/smashit/core/usecases/player/FindByIdUseCaseImpl.java
@@ -1,8 +1,16 @@
 package com.totex.smashit.core.usecases.player;
 
 import com.totex.smashit.core.entities.player.Player;
+import com.totex.smashit.core.gateway.player.PlayerGateway;
 
 public class FindByIdUseCaseImpl implements FindByIdUseCase {
+
+    private final PlayerGateway playerGateway;
+
+    public FindByIdUseCaseImpl(PlayerGateway playerGateway) {
+        this.playerGateway = playerGateway;
+    }
+
     @Override
     public Player findById(Long id) {
         return null;
