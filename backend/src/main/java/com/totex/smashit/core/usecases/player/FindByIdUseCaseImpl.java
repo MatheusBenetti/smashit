2afea9e9f9b0a4e2766ff3c0cfe522916d579a7a
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-11-19 09:17:31 -0300

    add logger and implement

--- backend/src/main/java/com/totex/smashit/core/usecases/player/FindByIdUseCaseImpl.java
+++ backend/src/main/java/com/totex/smashit/core/usecases/player/FindByIdUseCaseImpl.java
@@ -3,7 +3,12 @@ package com.totex.smashit.core.usecases.player;
 import com.totex.smashit.core.entities.player.Player;
 import com.totex.smashit.core.gateway.player.PlayerGateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FindByIdUseCaseImpl implements FindByIdUseCase {
+
+    private static final Logger log = LoggerFactory.getLogger(FindByIdUseCaseImpl.class);
 
     private final PlayerGateway playerGateway;
 
@@ -13,6 +18,14 @@ public class FindByIdUseCaseImpl implements FindByIdUseCase {
 
     @Override
     public Player findById(Long id) {
-        return null;
+        log.info("Finding player with ID: {}", id);
+        try {
+            Player found = playerGateway.findById(id);
+            log.info("Player found: {}", found);
+            return found;
+        } catch (Exception e) {
+            log.error("Error while trying to find player. Error: {}", e.getMessage());
+            throw e;
+        }
     }
 }
