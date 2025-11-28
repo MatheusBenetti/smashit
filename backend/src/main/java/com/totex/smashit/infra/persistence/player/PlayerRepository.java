4460d52ab513488956ebc27f00f733ee8ffa193a
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-11-28 10:04:06 -0300

    add find by name ignore case to repository

--- backend/src/main/java/com/totex/smashit/infra/persistence/player/PlayerRepository.java
+++ backend/src/main/java/com/totex/smashit/infra/persistence/player/PlayerRepository.java
@@ -1,8 +1,12 @@
 package com.totex.smashit.infra.persistence.player;
 
+import com.totex.smashit.core.entities.player.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
+    Optional<Player> findByNameIgnoreCase(String name);
 }
