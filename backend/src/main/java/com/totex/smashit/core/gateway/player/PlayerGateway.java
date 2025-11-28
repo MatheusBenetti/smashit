5884fbc2c7e84414ff03340f0ce34507b6d04743
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-11-28 10:04:53 -0300

    add methods to player gateway

--- backend/src/main/java/com/totex/smashit/core/gateway/player/PlayerGateway.java
+++ backend/src/main/java/com/totex/smashit/core/gateway/player/PlayerGateway.java
@@ -3,6 +3,7 @@ package com.totex.smashit.core.gateway.player;
 import com.totex.smashit.core.entities.player.Player;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PlayerGateway {
 
@@ -10,4 +11,6 @@ public interface PlayerGateway {
     List<Player> findPlayer();
     Player updatePlayer(Long id, Player player);
     Player findById(Long id);
+    Optional<Player> findByName(String name);
+    boolean existsByName(String name);
 }
