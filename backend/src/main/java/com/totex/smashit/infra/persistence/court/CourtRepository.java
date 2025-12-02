dbce257f252bf63ab0f141a616a5ed022187980f
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-12-02 12:27:46 -0300

    add find by name ignore case on court repository

--- backend/src/main/java/com/totex/smashit/infra/persistence/court/CourtRepository.java
+++ backend/src/main/java/com/totex/smashit/infra/persistence/court/CourtRepository.java
@@ -1,8 +1,12 @@
 package com.totex.smashit.infra.persistence.court;
 
+import com.totex.smashit.core.entities.court.Court;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CourtRepository extends JpaRepository<CourtEntity, Long> {
+    Optional<Court> findByNameIgnoreCase(String name);
 }
