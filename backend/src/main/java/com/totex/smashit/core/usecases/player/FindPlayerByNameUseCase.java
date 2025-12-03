2135ad8ee0fdb00d9ea62a39711d71002ca7a91a
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2025-12-03 15:03:23 -0300

    rename method

--- backend/src/main/java/com/totex/smashit/core/usecases/player/FindPlayerByNameUseCase.java
+++ backend/src/main/java/com/totex/smashit/core/usecases/player/FindPlayerByNameUseCase.java
@@ -3,5 +3,5 @@ package com.totex.smashit.core.usecases.player;
 import com.totex.smashit.core.entities.player.Player;
 
 public interface FindPlayerByNameUseCase {
-    public Player findPlayerByName(String name);
+    public Player execute(String name);
 }
